2c96227fcf0731da2daebf7a490c26e1459f2436
xujun540 <17683768715@163.com>
2022-03-09 16:52:49 +0800

    chore(autotest) add jwk test server

---
+++ jwkserver/src/main/java/com/emqx/jwkserver/SvtserverApplication.java
@@ -0,0 +1,13 @@
+package com.emqx.jwkserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvtserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvtserverApplication.class, args);
+	}
+
+}
